c1b7a7b8041160e36cb76603a4600de41a1e220f
Maryse <maryseokana@gmail.com>
2025-12-30 21:17:54 +0000

    Fix login route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Security;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,5 +19,8 @@ class LoginController extends AbstractController
     }
 
     #[Route('/logout', name: 'app_logout')]
-    public function logout(): void {}
+    public function logout(): void
+    {
+        
+    }
 }
